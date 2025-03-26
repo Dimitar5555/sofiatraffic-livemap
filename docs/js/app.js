15b4d329e4de2efb41cb9feb659569ff6987df5d
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-03-26 10:22:34 +0200

    Update map tiles url

--- docs/js/app.js
+++ docs/js/app.js
@@ -88,7 +88,7 @@ function init_map() {
         zoom: 13
     });
     map.invalidateSize();
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
     
 }
