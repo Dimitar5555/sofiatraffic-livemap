50ce5e0d5c0f4f688f137f67d70aadd8c9ed87d6
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-07-13 15:25:08 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const MERGE_TRAM_COMPONENTS = true;
 export const MIN_ACTIVE_SPEED = 5;
-export const WEBSOCKET_URL = 'wss://damp-unit-e9e4.bulgariadb.workers.dev/';
+export const WEBSOCKET_URL = 'wss://damp-unit-e9e4.bulgariadb.workers.dev/websocket/';
 export const DEBUG_MODE = false;
 export const BG_TYPES = {
     'tram': 'Трамвай',
