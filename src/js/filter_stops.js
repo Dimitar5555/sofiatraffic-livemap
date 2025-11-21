467edcc2a1d377bc5da651949ff46a3a0a393498
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-11-21 10:24:39 +0200

    refactor stop filtering

--- src/js/filter_stops.js
+++ src/js/filter_stops.js
@@ -2,19 +2,19 @@ import { map } from './map.js';
 import { stops } from './map_stops.js';
 import { is_screen_width_lg_or_less } from './app.js';
 
-function filter_by_name(term) {
+function filter_stops(term) {
     const matches = [];
+    const max_shown_stops = 15;
     term = term.trim().toLowerCase();
     const lacks_term = term.length == 0;
-    let checked = 0;
     for(const [code, stop] of stops.entries()) {
         const name = stop.names.bg.toLowerCase();
-        const is_match = name.toLowerCase().includes(term) || stop.code.toString().toLowerCase().includes(term);
-        checked ++;
-        if(is_match || lacks_term) {
+        const name_match = name.includes(term);
+        const code_match = stop.code.toString().padStart(4, '0').includes(term);
+        if(name_match || code_match || lacks_term) {
             matches.push(code);
         }
-        if(matches.length >= 10) {
+        if(matches.length >= max_shown_stops) {
             break;
         }
     }
@@ -24,7 +24,7 @@ function filter_by_name(term) {
 window.update_stop_suggestions = function() {
     const old_tbody = document.querySelector('table#stops_table > tbody');
     const term = document.querySelector('#stop_name_filter').value;
-    const matches = filter_by_name(term);
+    const matches = filter_stops(term);
     console.log(term, matches);
     const new_tbody = document.createElement('tbody');
     for(const code of matches) {
